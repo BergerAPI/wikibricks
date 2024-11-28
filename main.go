7a10eda362259fb50a7295c753f7a250f218fb92
BergerAPI <bergerbtw@gmail.com>
2024-11-28 21:59:42 +0100

    added go fiber

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal("Fiber: Failed to start server.")
+	}
 }
