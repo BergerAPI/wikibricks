a400d8b5ec34866b7d1b20a2213829d826188787
BergerAPI <bergerbtw@gmail.com>
2024-11-29 14:31:25 +0100

    feat: basic database

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
 	"log"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Connecting to the database
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:password@localhost:5432")
+	if err != nil {
+		log.Fatal("Unable to connect to database")
+	}
+
+	// CLosing the connection when the application is shut down
+	defer func(conn *pgx.Conn, ctx context.Context) {
+		if err := conn.Close(ctx); err != nil {
+			log.Fatal("Failed to close connection.")
+		}
+	}(conn, context.Background())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
---
+++ migration/001_initialize_tables.sql
@@ -0,0 +1,13 @@
+create table if not exists t_brand(
+    id serial primary key,
+    name varchar(255),
+    description text
+);
+
+create table if not exists t_set(
+    id serial primary key,
+    name varchar(255),
+    description text,
+    pieces integer,
+    brand_id integer references t_brand(id)
+);
