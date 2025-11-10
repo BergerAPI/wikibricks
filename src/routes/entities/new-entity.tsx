3017a6b041b96c98c83110cffb53b9e808f3e175
BergerAPI <bergerbtw@gmail.com>
2025-11-10 12:03:04 +0100

    fix: unchanged code

--- src/routes/entities/new-entity.tsx
+++ src/routes/entities/new-entity.tsx
@@ -1,76 +1,109 @@
-import { INFO_FIELDS, type EntityAttributes } from ".";
+import { getInfoFields, type EntityAttributes } from ".";
 import { sql, type EntityType } from "../../database";
 import { Layout } from "../../layout";
+import { useTranslation } from "../../translation";
 import type { DefaultContext } from "../../utils";
 
 export const handleNewEntityPage = async (c: DefaultContext) => {
-    const user = c.get("user");
+  const user = c.get("user");
+  const { t } = useTranslation(c);
 
-    if (user === undefined) return c.redirect("/login?redirect=/entities/new");
+  if (user === undefined) return c.redirect("/login?redirect=/entities/new");
 
-    return c.render(
-        <Layout user={user}>
-            <main>
-                <h1 class="text-3xl font-serif pb-2 mb-3 border-b">Add New Entity</h1>
+  const infoFields = getInfoFields(t);
 
-                <div id="error" class="empty:hidden mb-3 p-3 border border-red-500 bg-red-100 text-red-700"></div>
+  return c.render(
+    <Layout context={c} user={user}>
+      <main>
+        <h1 class="text-3xl font-serif pb-2 mb-3 border-b">Add New Entity</h1>
 
-                <div>
-                    <label class="block mb-3">
-                        <span class="font-semibold">Entity Type</span>
-                        <select class="mt-1 block w-full" field="entity_type" name="brand_id" value="set" id="entity_type">
-                            {Object.keys(INFO_FIELDS).map(id => <option value={id}>
-                                {id.at(0)?.toUpperCase() + id.slice(1, id.length).toLowerCase()}
-                            </option>)}
-                        </select>
+        <div
+          id="error"
+          class="empty:hidden mb-3 p-3 border border-red-500 bg-red-100 text-red-700"
+        ></div>
+
+        <div>
+          <label class="block mb-3">
+            <span class="font-semibold">Entity Type</span>
+            <select
+              class="mt-1 block w-full"
+              field="entity_type"
+              name="brand_id"
+              value="set"
+              id="entity_type"
+            >
+              {Object.keys(infoFields).map((id) => (
+                <option value={id}>
+                  {id.at(0)?.toUpperCase() +
+                    id.slice(1, id.length).toLowerCase()}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <label class="block mb-3">
+            <span class="font-semibold">Name</span>
+            <input
+              id="name"
+              field="name"
+              value=""
+              class="border p-2 rounded w-full"
+            />
+          </label>
+
+          <label class="block mb-3">
+            <span class="font-semibold">Description</span>
+            <textarea
+              field="description"
+              id="description"
+              value=""
+              class="border p-2 rounded w-full"
+            />
+          </label>
+
+          {/* Tab contents */}
+          {Object.keys(infoFields).map((id, index) => (
+            <div
+              key={id}
+              id={`tab-${id}`}
+              class={`tab-content ${index === 0 ? "" : "hidden"}`}
+            >
+              {Object.entries(infoFields[id as keyof typeof infoFields]).map(
+                ([key, def]) => {
+                  let fieldLabel = "";
+                  let inputType: "text" | "number" = "text";
+
+                  if (typeof def === "string") {
+                    fieldLabel = def;
+                  } else {
+                    fieldLabel = def.text;
+                    inputType = def.type === "number" ? "number" : "text";
+                  }
+
+                  return (
+                    <label htmlFor={key} class="block mb-3">
+                      <span class="font-semibold">{fieldLabel}</span>
+
+                      <input
+                        id={key}
+                        field={key}
+                        type={inputType}
+                        value={""}
+                        class="border p-2 rounded w-full"
+                      />
                     </label>
+                  );
+                },
+              )}
+            </div>
+          ))}
 
-                    <label class="block mb-3">
-                        <span class="font-semibold">Name</span>
-                        <input id="name" field="name" value="" class="border p-2 rounded w-full" />
-                    </label>
+          <button id="submit_button">Submit</button>
+        </div>
 
-                    <label class="block mb-3">
-                        <span class="font-semibold">Description</span>
-                        <textarea field="description" id="description" value="" class="border p-2 rounded w-full" />
-                    </label>
-
-                    {/* Tab contents */}
-                    {Object.keys(INFO_FIELDS).map((id, index) => (
-                        <div key={id} id={`tab-${id}`} class={`tab-content ${index === 0 ? "" : "hidden"}`}>
-                            {Object.entries(INFO_FIELDS[id as keyof typeof INFO_FIELDS]).map(([key, def]) => {
-                                let fieldLabel = "";
-                                let inputType: "text" | "number" = "text";
-
-                                if (typeof def === "string") {
-                                    fieldLabel = def;
-                                } else {
-                                    fieldLabel = def.text;
-                                    inputType = def.type === "number" ? "number" : "text";
-                                }
-
-                                return (
-                                    <label htmlFor={key} class="block mb-3">
-                                        <span class="font-semibold">{fieldLabel}</span>
-
-                                        <input
-                                            id={key}
-                                            field={key}
-                                            type={inputType}
-                                            value={""}
-                                            class="border p-2 rounded w-full"
-                                        />
-                                    </label>
-                                );
-                            })}
-                        </div>
-                    ))}
-
-                    <button id="submit_button">Submit</button>
-                </div>
-
-                <script dangerouslySetInnerHTML={{
-                    __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
 document.getElementById("submit_button").addEventListener("click", () => {
     const data = {};
     // Only include fields that are visible (i.e. not in a hidden tab)
@@ -117,39 +150,51 @@ document.getElementById("entity_type").addEventListener("change", () => {
     const content = document.getElementById("tab-" + selected);
     if(content) content.classList.remove('hidden');
 });
-                    `
-                }} />
-            </main>
-        </Layout>,
-    );
+                    `,
+          }}
+        />
+      </main>
+    </Layout>,
+  );
 };
 
 export const handleNewEntitySubmit = async (c: DefaultContext) => {
-    const user = c.get("user");
+  const user = c.get("user");
+  const { t } = useTranslation(c);
 
-    if (!user) return c.body("Unauthorized", 401);;
+  const infoFields = getInfoFields(t);
 
-    const { name, description, entity_type, ...data } = await c.req.json();
+  if (!user) return c.body("Unauthorized", 401);
 
-    if (!name || !description || !entity_type || name.length < 3 || description.length < 3) {
-        return c.body("Name and description are required and need to be at least 3 characters long.", 400);
-    }
+  const { name, description, entity_type, ...data } = await c.req.json();
 
-    // Checking whether the necessary fields of the specific entity type have been supplied
-    const typeInfo = INFO_FIELDS[entity_type as keyof typeof INFO_FIELDS];
+  if (
+    !name ||
+    !description ||
+    !entity_type ||
+    name.length < 3 ||
+    description.length < 3
+  ) {
+    return c.body(
+      "Name and description are required and need to be at least 3 characters long.",
+      400,
+    );
+  }
 
-    if (!typeInfo) return c.body("Bad Request", 400);
+  // Checking whether the necessary fields of the specific entity type have been supplied
+  const typeInfo = infoFields[entity_type as keyof typeof infoFields];
 
-    for (const [fieldKey] of Object.entries(typeInfo)) {
-        if (data[fieldKey] !== undefined)
-            continue
+  if (!typeInfo) return c.body("Bad Request", 400);
 
-        return c.body("Bad Request", 400)
-    }
+  for (const [fieldKey] of Object.entries(typeInfo)) {
+    if (data[fieldKey] !== undefined) continue;
 
-    // Every created set by a user will be handles by a change and needs to be accepted by a moderator
-    const version = await sql.begin(async sql => {
-        const [{ id, entity_id }] = await sql<{ id: number, entity_id: number }[]>`
+    return c.body("Bad Request", 400);
+  }
+
+  // Every created set by a user will be handles by a change and needs to be accepted by a moderator
+  const version = await sql.begin(async (sql) => {
+    const [{ id, entity_id }] = await sql<{ id: number; entity_id: number }[]>`
             WITH new_entity AS (
                 INSERT INTO entities (name, type)
                 VALUES (${name.toString()}, ${entity_type})
@@ -173,26 +218,25 @@ export const handleNewEntitySubmit = async (c: DefaultContext) => {
             ) SELECT entity_id, id FROM new_version;
         `;
 
-        // Handling creation of the specific entity tables
-        if (entity_type === "set") {
-            await sql`
+    // Handling creation of the specific entity tables
+    if (entity_type === "set") {
+      await sql`
                 INSERT INTO sets ${sql({
-                version_id: id,
-                ...data
-            })}`;
-        } else if (entity_type === "brand") {
-            await sql`
+                  version_id: id,
+                  ...data,
+                })}`;
+    } else if (entity_type === "brand") {
+      await sql`
                 INSERT INTO brands ${sql({
-                version_id: id,
-                ...data
-            })}`;
-        } else if (entity_type === "wiki") {
-            // No additional table insertion is needed for wiki entities.
-        }
+                  version_id: id,
+                  ...data,
+                })}`;
+    } else if (entity_type === "wiki") {
+      // No additional table insertion is needed for wiki entities.
+    }
 
-        return { id, entity_id }
-    })
+    return { id, entity_id };
+  });
 
-
-    return c.json(version)
+  return c.json(version);
 };
